926f468b62817ac804002e1c02e610d9f9f08e19
benjamin <vinh.sam@gmail.com>
2014-10-22 00:04:23 +0700

    Lesson 1: Create Project Sunshine

--- app/src/main/java/com/vination/sunshine/MainActivity.java
+++ app/src/main/java/com/vination/sunshine/MainActivity.java
@@ -1,15 +1,18 @@
 package com.vination.sunshine;
 
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBarActivity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.os.Build;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.Arrays;
+import java.util.List;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -60,6 +63,26 @@ public class MainActivity extends ActionBarActivity {
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+            List<String> entries = Arrays.asList(
+                    "Today -- Sunny -- 88/63",
+                    "Tomorrow -- Foggy -- 70/46",
+                    "Weds -- Cloudy -- 72/63",
+                    "Thurs -- Rainy -- 64/51",
+                    "Fri -- Foggy -- 70/46",
+                    "Sat -- Sunny -- 76/68"
+            );
+
+            ListView listView = (ListView) rootView.findViewById(R.id.listview_forecast);
+            listView.setAdapter(
+                    new ArrayAdapter<String>(
+                            getActivity(),
+                            R.layout.list_item_forecast,
+                            R.id.list_item_forecast_textview,
+                            entries
+                    )
+            );
+
             return rootView;
         }
     }
